3aa846858a64e3dbf98565f883195cfa0a8266db
Mattias <mattias.lemmens@showtex.com>
2017-06-30 10:24:22 +0200

    Delete example_GL_sum.js

--- example_GL_sum.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(company,GL_no,interco_code){
-return " "/*--example starts here*/+
-"select sum(Amount) as Amount from ["+company+"$G_L Entry] "/*--table_name param company*/+
-"where [G_L Account No_] like '"+GL_no+"' "/*--switched for GL_no variable*/+
-"and [Global Dimension 1 Code] in ("+interco_code+") "/*--switched for Interco_code variable*/+
-" "/*--end*/+
-}
